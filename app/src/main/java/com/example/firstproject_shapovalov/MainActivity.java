0a8d281c1b65cc2a77a90184c7b5924f9492a0b0
dima <balecored@gmail.com>
2026-02-20 23:05:57 +0500

    Разработанный переход. Шаповалов

--- app/src/main/java/com/example/firstproject_shapovalov/MainActivity.java
+++ app/src/main/java/com/example/firstproject_shapovalov/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.firstproject_shapovalov;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +21,16 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void onStepAge(View view){
+        setContentView(R.layout.step_2);
+    }
+
+    public void onStepSex(View view){
+        setContentView(R.layout.step_3);
+    }
+    public void onStepEmail(View view){
+        setContentView(R.layout.step_4);
     }
 }
